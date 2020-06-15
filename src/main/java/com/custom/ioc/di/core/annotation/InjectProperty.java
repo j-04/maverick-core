3b52993f447253ef163efcf26c77e4b475dbc1ae
Jonathan <dragoshs.j@yahoo.com>
2020-06-15 22:34:39 +0300

    Implemented annotations for di and other configuration features
    - InjectByType annotation
    - InjectProperty annotation
    - Singleton annotation
    - Lazy annotation

---
+++ src/main/java/com/custom/ioc/di/core/annotation/InjectProperty.java
@@ -0,0 +1,9 @@
+package com.custom.ioc.di.core.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface InjectProperty {
+    String value() default "";
+}
